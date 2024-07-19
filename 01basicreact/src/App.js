0ddf33f1fd67d724fc8a81ff259fa546960f0785
THESV7 <vadhelsmit@gmail.com>
2024-07-19 15:36:00 +0530

    Understand the react flow and structure

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,10 +1,17 @@
-
+import Chai from "./Chai";
 
 function App() {
   return (
+    <>
+    <Chai />
     <h1>chai aur react | SV</h1>
+    </>
   );
 }
 
 export default App;
 
+/*
+functions name should be capital 
+
+*/
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,14 @@
-
-
-
-
+import Chai from "./chai"
 
 function App() {
 
   return (
-    <h1>Chai aur react with vite | SV</h1>
+    <>
+    <Chai />
+    <h1>chai aur react</h1>
+    <p>test para</p>
+    </>
   )
 }
 
-export default App
+export default App;
