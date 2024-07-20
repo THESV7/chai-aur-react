c8fa001e5a57e2644e1317ffb340b968ed61f42f
THESV7 <vadhelsmit@gmail.com>
2024-07-20 15:14:26 +0530

    Create own react library and JSX

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
 import Chai from "./chai"
 
 function App() {
+  const username = "chai aur code"
 
   return (
     <>
     <Chai />
-    <h1>chai aur react</h1>
+    <h1>chai aur react {username}</h1> {/*{evaluation expression}*/}
     <p>test para</p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,7 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App | chai</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://www.google.com',
+//         target: '_blank'
+//     },
+//     childern: 'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const anotherUser = " chai aur code"
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'Click me to visit google',
+    anotherUser
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    // MyApp()
+    // <App />
+    // anotherElement
+    reactElement
 )
